ce289b7c3c779a9f0b691efe5d88d0cd45debdc4
Jan Klötzke <jan@kloetzke.net>
2024-12-04 22:51:17 +0100

    doc: use Read the Docs theme

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@ author = 'Jan Klötzke'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ 'sphinx.ext.intersphinx' ]
+extensions = [ 'sphinx.ext.intersphinx', 'sphinx_rtd_theme' ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -27,5 +27,5 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
